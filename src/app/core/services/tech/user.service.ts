66ebbacee4f5d8294365c80acf20b6de162b6b96
Xavier CLEMENCE <xavier.clemence@gmail.com>
2019-03-24 20:09:31 +0100

    try force transform ts

--- src/app/core/services/tech/user.service.ts
+++ src/app/core/services/tech/user.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
+import { User } from '@app/Core/models/user';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
-
-import { User } from './../../models/user';
 
 @Injectable({
   providedIn: 'root'
