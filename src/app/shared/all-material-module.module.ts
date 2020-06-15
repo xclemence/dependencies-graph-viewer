b5a26a5e575e2836ca7f6a5e1f8bfd5aa9f40b48
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-06-16 00:19:04 +0200

    Fix some issues design

--- src/app/shared/all-material-module.module.ts
+++ src/app/shared/all-material-module.module.ts
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
@@ -16,12 +17,11 @@ import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   imports: [MatToolbarModule, MatListModule, MatCardModule, MatTabsModule, MatInputModule, MatCheckboxModule,
     MatIconModule, MatSnackBarModule, MatSidenavModule, MatTableModule, MatDialogModule, ScrollingModule,
-    MatSliderModule, MatSortModule, MatButtonModule, MatMenuModule, MatTooltipModule, MatPaginatorModule],
+    MatSliderModule, MatSortModule, MatButtonModule, MatMenuModule, MatTooltipModule, MatPaginatorModule ],
   exports: [MatToolbarModule, MatListModule, MatCardModule, MatTabsModule, MatInputModule, MatCheckboxModule,
     MatIconModule, MatSnackBarModule, MatSidenavModule, MatTableModule, MatDialogModule, ScrollingModule,
     MatSliderModule, MatSortModule, MatButtonModule, MatMenuModule, MatTooltipModule, MatPaginatorModule],
