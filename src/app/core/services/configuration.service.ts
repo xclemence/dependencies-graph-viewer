6411816881346d75a08e2510dde1e0097523a847
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-06 01:16:46 +0200

    Fix loading json config

--- src/app/core/services/configuration.service.ts
+++ src/app/core/services/configuration.service.ts
@@ -1,21 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import * as data from '@app/../assets/config.json';
 import { environment } from 'environments/environment';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigurationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  load(): boolean {
+  load(): Promise<boolean> | boolean {
 
-    if (environment.production)
-    {
-      const configuration = (data as any).default;
-      environment.assemblyGraphqlUri = configuration.assemblyGraphqlUri;
+    if (environment.production) {
+      return this.http.get('./assets/config.json')
+        .toPromise()
+        .then((config: any) => {
+          environment.assemblyGraphqlUri = config.assemblyGraphqlUri;
+          return true;
+        }).catch((err) => false);
     }
     return true;
   }
