8af8c47362590fbb36bbabcb1160d3c313fc8175
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-07 01:31:38 +0200

    Add chrome headless

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,5 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 
 module.exports = function (config) {
   config.set({
@@ -25,7 +28,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeNoSandbox'],
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
