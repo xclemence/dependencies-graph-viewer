bd38edc2bb6d327562624bbdd639052114574da3
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-10 00:08:01 +0200

    update ci config

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,33 +1,3 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
+var config = exports.config = require('./protractor.conf.js').config;
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-process.env.CHROME_BIN = process.env.CHROME_BIN || require('puppeteer').executablePath();
-
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome',
-    chromeOptions: {
-      binary: process.env.CHROME_BIN,
-      args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
-    },
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
-};
+config.capabilities.chromeOptions.args = ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox'];
