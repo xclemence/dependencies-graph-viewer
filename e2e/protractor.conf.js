d26b1e7a3dd7803046e8429c22b304b3efd1a0d8
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-11 12:39:36 +0200

    use os chrome for test

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,7 +2,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+// process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 exports.config = {
   allScriptsTimeout: 11000,
