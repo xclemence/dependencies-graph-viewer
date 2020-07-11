9f1149e5e06130502d5a0c5bc8116774d2f187ea
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-11 12:52:41 +0200

    New config

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,3 +1,18 @@
-var config = exports.config = require('./protractor.conf.js').config;
-
-config.capabilities.chromeOptions.args = ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox', "--disable-dev-shm-usage"];
+const config = require('./protractor.conf').config;  
+  
+config.capabilities = {  
+  browserName: 'chrome',  
+  chromeOptions: {  
+    args: [  
+      '--headless',  
+      '--no-sandbox',  
+      '--disable-gpu',  
+      '--disable-dev-shm-usage',  
+      '--ignore-certificate-errors',  
+      '--window-size=1920,1080'  
+    ],  
+    binary: require('puppeteer').executablePath()  
+  }
+};  
+  
+exports.config = config;
