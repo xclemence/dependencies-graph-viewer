ccd2ee589a47dd176674c9d753a78252f08a6ae7
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-11 11:49:44 +0200

    Change config

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,3 +1,3 @@
 var config = exports.config = require('./protractor.conf.js').config;
 
-config.capabilities.chromeOptions.args = ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox'];
+config.capabilities.chromeOptions.args = ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox', "--remote-debugging-port=9222"];
