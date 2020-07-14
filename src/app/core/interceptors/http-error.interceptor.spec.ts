4abde8607dc62e406224fb65819a2da0aeac12e0
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-14 23:06:55 +0200

    Add error interceptor tests

--- src/app/core/interceptors/http-error.interceptor.spec.ts
+++ src/app/core/interceptors/http-error.interceptor.spec.ts
@@ -1,23 +1,62 @@
-import { TestBed } from '@angular/core/testing';
-import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
+import { throwError } from 'rxjs';
 
+import { operationFailure } from '../store/actions';
+import { CoreState } from '../store/models';
 import { HttpErrorInterceptor } from './http-error.interceptor';
 
+describe('HttpErrorInterceptor with ErrorEvent', () => {
+  let httpRequestSpy: any;
+  let httpHandlerSpy: any;
+  let storeSpy: any;
 
-describe('HttpErrorInterceptor', () => {
-
-  const initialState = { test: false };
-
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      HttpErrorInterceptor,
-      provideMockStore({ initialState }),
-      ]
-  }));
-
-  it('should be created', () => {
-    const interceptor: HttpErrorInterceptor = TestBed.inject(HttpErrorInterceptor);
-    const store = TestBed.inject(MockStore);
-    expect(interceptor).toBeTruthy();
+  beforeEach(() => {
+    httpRequestSpy = jasmine.createSpyObj('HttpRequest', ['doesNotMatter']);
+    httpHandlerSpy = jasmine.createSpyObj('HttpHandler', ['handle']);
+    storeSpy = jasmine.createSpyObj<Store<CoreState>>('store', ['dispatch']);
   });
+
+  fit('should dispatch error message from Error', () => {
+
+    const expectedActionOnError = operationFailure({ error: 'Error (404): test-error', origin: undefined });
+
+    httpHandlerSpy.handle.and.returnValue(throwError(
+      {
+        status: 404,
+        message: 'test-error'
+      }
+    ));
+
+    const errorInterceptor = new HttpErrorInterceptor(storeSpy);
+
+    errorInterceptor.intercept(httpRequestSpy, httpHandlerSpy)
+      .subscribe(
+        result => console.log('good', result),
+        err => {
+          expect(err).toEqual('Error (404): test-error');
+          expect(storeSpy.dispatch).toHaveBeenCalledWith(expectedActionOnError);
+        }
+      );
+
+  });
+
+  fit('should dispatch error message from ErrorEvent', () => {
+    const expectedErrorMessage = 'Error: test-error';
+    const expectedActionOnError = operationFailure({ error: expectedErrorMessage, origin: undefined });
+
+    const error = new ErrorEvent('Error', { message: 'test-error' });
+    httpHandlerSpy.handle.and.returnValue(throwError({ error }));
+
+    const errorInterceptor = new HttpErrorInterceptor(storeSpy);
+
+    errorInterceptor.intercept(httpRequestSpy, httpHandlerSpy)
+      .subscribe(
+        result => console.log('good', result),
+        err => {
+          expect(err).toEqual(expectedErrorMessage);
+          expect(storeSpy.dispatch).toHaveBeenCalledWith(expectedActionOnError);
+        }
+      );
+  });
+
 });
