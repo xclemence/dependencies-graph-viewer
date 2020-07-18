8502d627819bcc1018cd4d53503fdb6ef58d4ea3
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-07-18 14:27:28 +0200

    New graph ut

--- src/app/shared/components/force-graph/force-graph.component.spec.ts
+++ src/app/shared/components/force-graph/force-graph.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { GraphLink, GraphNode } from '@app/shared/models';
 
-import { ForceGraphComponent } from './force-graph.component';
+import { ForceGraphComponent, GraphUpdateMode } from './force-graph.component';
 
 describe('ForceGraphComponent', () => {
   let component: ForceGraphComponent;
@@ -58,6 +58,99 @@ describe('ForceGraphComponent', () => {
 
     expect(fixture.debugElement.queryAll(By.css('circle')).length).toBe(2);
     expect(fixture.debugElement.queryAll(By.css('line')).length).toBe(1);
+  });
+
+
+  it('should not re create graph if same graph object', () => {
+    const graph = {
+      nodes: [
+        new GraphNode({ id: '1', label: 'node1', color: 'red' }),
+      ],
+      links: [ ]
+    };
+
+    component.graph = graph;
+    fixture.detectChanges();
+
+    graph.nodes.push(new GraphNode({ id: '3', label: 'node3' }));
+
+    component.graph = graph; // No Changed
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('circle')).length).toBe(1);
+  });
+
+  it('should update graph', () => {
+    const baseGraph = {
+      nodes: [
+        new GraphNode({ id: '1', label: 'node1', color: 'red' }),
+      ],
+      links: [ ]
+    };
+
+    component.graph = baseGraph;
+    fixture.detectChanges();
+
+    const newGraph = {
+      nodes: [
+        new GraphNode({ id: '1', label: 'node1', color: 'red' }),
+        new GraphNode({ id: '2', label: 'node2', color: 'red' }),
+      ],
+      links: [
+        new GraphLink({source: '1', target: '2'})
+       ]
+    };
+    component.graph = newGraph;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('circle')).length).toBe(2);
+    expect(fixture.debugElement.queryAll(By.css('line')).length).toBe(1);
+  });
+
+  it('should update graph whith clean mode', () => {
+    const baseGraph = {
+      nodes: [
+        new GraphNode({ id: '1', label: 'node1', color: 'red' }),
+      ],
+      links: [ ]
+    };
+
+    component.updateMode = GraphUpdateMode.ClearAndAdd;
+    component.graph = baseGraph;
+    fixture.detectChanges();
+
+    const newGraph = {
+      nodes: [
+        new GraphNode({ id: '1', label: 'node1', color: 'red' }),
+        new GraphNode({ id: '2', label: 'node2', color: 'red' }),
+      ],
+      links: [
+        new GraphLink({source: '1', target: '2'})
+       ]
+    };
+    component.graph = newGraph;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('circle')).length).toBe(2);
+    expect(fixture.debugElement.queryAll(By.css('line')).length).toBe(1);
+  });
+
+  it('should remove graph', () => {
+    const baseGraph = {
+      nodes: [
+        new GraphNode({ id: '1', label: 'node1', color: 'red' }),
+      ],
+      links: [ ]
+    };
+
+    component.graph = baseGraph;
+    fixture.detectChanges();
+
+    component.graph = null;
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('circle')).length).toBe(0);
+    expect(fixture.debugElement.queryAll(By.css('line')).length).toBe(0);
   });
 
 });
