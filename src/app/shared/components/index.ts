23d7cdbe6d99e5d6720bb8b103942871fd19676f
Xavier CLEMENCE <32803123+xclemence@users.noreply.github.com>
2021-07-14 17:18:03 +0200

    Review design

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -4,5 +4,4 @@ export * from './force-graph/force-graph.component';
 export * from './three-force-graph/three-force-graph.component';
 
 export * from './header/header-links/header-links.component';
-export * from './header/header-previous/header-previous.component';
 export * from './header/header-user/header-user.component';
